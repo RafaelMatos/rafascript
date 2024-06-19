01f390b1f363594d0a3617835ebd313f0698bc07
RafaelMatos <rafael-matos@outlook.com>
2024-06-19 19:11:00 -0300

    style: change default color on MagicButton component

--- app/components/ui/MagicButton.tsx
+++ app/components/ui/MagicButton.tsx
@@ -16,7 +16,7 @@ const MagicButton = ({title,icon,position,handleClick,otherClasses,whatsapp=fals
      rounded-lg p-[1px] focus:outline-none md:w-60"
      onClick={handleClick}
      >
-      <span className={`absolute inset-[-1000%] animate-[spin_2s_linear_infinite] ${whatsapp ? 'bg-[conic-gradient(from_90deg_at_50%_50%,#dcfce7_0%,#25d366_50%,#dcfce7_100%)]':'bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]'}`} />
+      <span className={`absolute inset-[-1000%] animate-[spin_2s_linear_infinite] ${whatsapp ? 'bg-[conic-gradient(from_90deg_at_50%_50%,#dcfce7_0%,#25d366_50%,#dcfce7_100%)]':'bg-[conic-gradient(from_90deg_at_50%_50%,#bfdbfe_0%,#075985_50%,#bfdbfe_100%)]'}`} />
       <span className={`inline-flex h-full w-full gap-2 cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 py-1 text-sm font-medium text-white backdrop-blur-3xl ${otherClasses}`}>
         {position === 'left' && icon}
         {title}
