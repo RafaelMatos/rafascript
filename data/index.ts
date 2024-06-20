21772b4e910fb2f9739347d4d4f001134b55875d
RafaelMatos <rafael-matos@outlook.com>
2024-06-20 11:23:04 -0300

    style: change BentoGrid Card Image

--- data/index.ts
+++ data/index.ts
@@ -10,10 +10,10 @@ export const gridItems = [
     id: 1,
     title: "Priorizo ​​a colaboração do cliente, promovendo a comunicação aberta",
     description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] ",
+    imgClassName: "w-full h-full [mask-image:linear-gradient(to_top,transparent,black_20%)]",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "/b2.jpg",
     spareImg: "",
   },
   {
